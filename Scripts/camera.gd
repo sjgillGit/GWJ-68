8e3f13ab8505e308ce835f46ec609d629087b773
Yak <bigfootmech@gmail.com>
2024-04-14 19:06:06 +0100

    Player_Movement added different way of assigning 'player' (% Value in Godot), that should be less of a hassle during level making

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,23 +1,25 @@
 extends Camera3D
 
 @export
-var player: CharacterBody3D
-@export
 var is_player_movement_adjusted: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	look_at(player.global_position)
+	var player = %Player
 	
-	var parent = get_parent_node_3d()
-	if parent is PathFollow3D:
-		if player.global_position.distance_to(global_position) > 5:
-			parent.progress += 0.05
-			pass
+	if player != null:
+		look_at(player.global_position)
+		
+		var parent = get_parent_node_3d()
+		if parent is PathFollow3D:
+			if player.global_position.distance_to(global_position) > 5:
+				parent.progress += 0.05
+				pass
 
 	pass
