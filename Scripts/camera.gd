023ab70120b7041cf51954115f5116a9f7e991d8
Yak <bigfootmech@gmail.com>
2024-04-14 23:47:32 +0100

    Player_Movement added a way for the camera to be "close" to the player without 'zooming cinematically'

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -2,6 +2,10 @@ extends Camera3D
 
 @export
 var is_player_movement_adjusted: bool = false
+@export
+var cinematic: bool = false
+@export
+var cinematic_progression_speed = 0.05
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,15 +15,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var player = %Player
+	var player: CharacterBody3D = %Player
 	
 	if player != null:
 		look_at(player.global_position)
 		
 		var parent = get_parent_node_3d()
+		
 		if parent is PathFollow3D:
-			if player.global_position.distance_to(global_position) > 5:
-				parent.progress += 0.05
-				pass
+			if cinematic:
+				if player.global_position.distance_to(global_position) > 5:
+					parent.progress += cinematic_progression_speed
+			else:
+				var path = parent.get_parent_node_3d()
+				parent.progress = path.curve.get_closest_offset(path.to_local(player.global_position))
+			pass
 
 	pass
